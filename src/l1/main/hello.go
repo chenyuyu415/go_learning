b9d82e310b9c3dd346c3644f4063518d596ad9c7
chenwenliang <chenwenliang@sz.hitrontech.com>
2021-03-15 11:25:05 +0800

    l1 finish

--- src/l1/main/hello.go
+++ src/l1/main/hello.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) == 2 {
+		fmt.Println("Hello Go!", os.Args[1])
+		os.Exit(0)
+	} else {
+		fmt.Println("More argues!", os.Args[1:])
+		os.Exit(1)
+	}
+}
